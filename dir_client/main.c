bef14c5811f0dbe72046078edf803b344a96ad75
Gabriel Sousa Lucas <gsousalucas@outlook.com>
2021-07-20 18:59:10 -0300

    client communicates with server

--- dir_client/main.c
+++ dir_client/main.c
@@ -1,14 +1,52 @@
 #include "../minitalk.h"
 
+void	end_mss(int pid)
+{
+	int	i;
+
+	i = 0;
+	while (i++ < 8)
+		kill(pid, SIGUSR2);
+}
+
+void	send_bits(char c, int pid)
+{
+	int	i;
+	int	buff[8];
+
+	i = 7;
+	while (i >= 0)
+	{
+		buff[i] = ((c & 1) > 0);
+		c >>= 1;
+		i--;
+	}
+	while (++i < 8)
+	{
+		if (buff[i])
+			kill(pid, SIGUSR1);
+		else
+			kill(pid, SIGUSR2);
+		usleep(100);
+	}
+}
+
 int	main(int argc, char **argv)
 {
 	int	pid;
+	int	i;
 
 	if (argc != 3)
 	{
 		write(1, "\033[1;31mError:\nInvaid number of arguments.\033[0m\n", 47);
 		return (-1);
 	}
+	i = 0;
 	pid = ft_atoi(argv[1]);
-	ft_putnmb(pid);
+	while (argv[2][i])
+	{
+		send_bits(argv[2][i], pid);
+		i++;
+	}
+	return (0);
 }
--- dir_server/main.c
+++ dir_server/main.c
@@ -1,17 +1,27 @@
 #include "../minitalk.h"
 
-void	sigrecmsg(int signum)
+void	handler_sig_receiver(int signal)
 {
-	write(1, "\033[0m\n", 3);
-	ft_putnmb(signum);
-	write(1, "\n", 1);
-	write(1, "EITA, FUI!!\n", 13);
+	static int	i;
+	static char	c;
+
+	if ((signal == SIGUSR1) && ((i % 8) > 0))
+		c = c << 1 | 1;
+	else if (signal == SIGUSR2)
+		c = c << 1;
+	else
+		c = c | 1;
+	i += 1;
+	if ((i % 8) == 0)
+	{
+		write(1, &c, 1);
+		c = 0;
+	}
 }
 
 int	main(int argc, char **argv)
 {
-	pid_t				pid;
-	struct sigaction	act;
+	pid_t	pid;
 
 	(void)argv;
 	if (argc != 1)
@@ -24,10 +34,8 @@ int	main(int argc, char **argv)
 	write(1, "\033[34mPID: ", 11);
 	ft_putnmb(pid);
 	write(1, "\n", 1);
-	act.sa_handler = sigrecmsg;
-	sigemptyset(&act.sa_mask);
-	act.sa_flags = SA_SIGINFO;
-	sigaction(SIGUSR1, &act, NULL);
+	signal(SIGUSR1, handler_sig_receiver);
+	signal(SIGUSR2, handler_sig_receiver);
 	while (1)
 		pause();
 	return (0);
